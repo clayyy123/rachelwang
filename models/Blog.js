9a7c34e01f3df368e22f4232819fb9d555cddf04
Clayton Wang <claydwang@gmail.com>
2018-05-18 14:34:54 -0700

    added token verification

---
+++ models/Blog.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose")
+
+const blogSchema = new mongoose.Schema({
+  user: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+  body: {type: string},
+  imageURL: {type:string}
+})
+
+const Blog = mongoose.model("Blog", blogSchema)
+module.exports = blog
--- server.js
+++ server.js
@@ -3,10 +3,22 @@ const app = express()
 const logger = require("morgan")
 const bodyParser = require("body-parser")
 const mongoose = require("mongoose")
-const PORT = 3001
+const PORT = process.env.load || 3001
+const dotenv = require("dotenv").load()
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/blog"
 
+mongoose.connect(MONGODB_URI, (err)=>{
+  console.log(err || "connected to mongodb")
+})
+
+app.use(logger("dev"))
+app.use(bodyParser.json())
+
+app.use('*', (req, res) => {
+	res.sendFile(`${__dirname}/client/build/index.html`)
+})
 
 
 app.listen(PORT, (err)=>{
-  console.log(err || `app running on port ${PORT}`)
+  console.log(err || `server running on port ${PORT}`)
 })
