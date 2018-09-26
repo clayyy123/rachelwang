e85906a884488416b054bce1e639642de97cfeab
Clayton Wang <claydwang@gmail.com>
2018-09-26 15:38:31 -0700

    css

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router, HashRouter } from 'react-router-dom'
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
+ReactDOM.render(<HashRouter><App /></HashRouter>, document.getElementById('root'));
 registerServiceWorker();
--- client/src/views/Footer.js
+++ client/src/views/Footer.js
@@ -1,18 +1,27 @@
-import React, {Component} from "react"
+import React, { Component } from 'react';
 
-
-class End extends Component{
-  render(){
-    return(
+class End extends Component {
+  render() {
+    return (
       <div className="Footer">
+        <ul>&copy; 2018 Rachel Wang Yoga</ul>
         <ul>
-          <a target="_blank" href="https://www.facebook.com/rachelywangyoga/"><li class="fab fa-facebook"></li></a>
-          <a target="_blank" href="https://www.instagram.com/rachely.wang/"><li class="fab fa-instagram"></li></a>
-          <a target="_blank" href="https://www.linkedin.com/in/rachel-wang-04657448/"><li class="fab fa-linkedin"></li></a>
+          <a target="_blank" href="https://www.facebook.com/rachelywangyoga/">
+            <li class="fab fa-facebook" />
+          </a>
+          <a target="_blank" href="https://www.instagram.com/rachely.wang/">
+            <li class="fab fa-instagram" />
+          </a>
+          <a
+            target="_blank"
+            href="https://www.linkedin.com/in/rachel-wang-04657448/"
+          >
+            <li class="fab fa-linkedin" />
+          </a>
         </ul>
       </div>
-    )
+    );
   }
 }
 
-export default End
+export default End;
